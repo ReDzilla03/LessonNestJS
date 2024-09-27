93ebf683a35f59622743022191625baa266c5a0b
Slavik Semenyuta <SemSlava2003@mail.ru>
2024-09-27 14:10:06 +0300

    Implementation logic module, controller and service, added Swagger documentation in Watchlist module

--- src/modules/users/models/user.model.ts
+++ src/modules/users/models/user.model.ts
@@ -1,7 +1,9 @@
-import { Column, Model, Table } from "sequelize-typescript";
+import { Column, HasMany, Model, Table } from "sequelize-typescript";
+import { Watchlist } from "src/modules/watchlist/models/watchlist.model";
 
 @Table
 export class User extends Model{
+
     @Column
     firstName: string
 
@@ -13,4 +15,10 @@ export class User extends Model{
 
     @Column
     password: string
+
+    @HasMany(() => Watchlist, {
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+    })
+    watchlist: Watchlist[]
 }
